9deed61c1d1d63111fbe1245ce06da49f34e6787
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-06 15:39:59 +0100

    Testing

--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("Account API")
+    res.send("Account API hello")
 })
 
 module.exports = router
