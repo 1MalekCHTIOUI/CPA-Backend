5c1b2d601335223a5f39c1dde4b1a3f57e05c1c0
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-14 15:06:55 +0100

    env issue

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const path = require('path')
 const accountRouter = require('./routes/account');
-
 app.use(cors());
 app.use(express.json());
-require("dotenv").config()
+require("dotenv").config({ path: path.resolve(__dirname, './.env') })
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
 .catch((err) => console.log(err));
-
 app.use('/account', accountRouter);
 
 
