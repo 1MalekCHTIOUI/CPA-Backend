4d881242eb8d07731881ccfe1db17dceaa386dcd
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-15 16:10:42 +0100

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 require("dotenv").config({ path: path.resolve(__dirname, './.env') })
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
-.catch((err) => console.log(err));
+.catch((err) => console.log("MongoDB Not Connected"))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
