8d2b59a96f2e75d63b713e6d196675d3dfd72dc5
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-15 15:44:40 +0100

    changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 const path = require('path')
 const authRouter = require('./routes/auth');
 app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 require("dotenv").config({ path: path.resolve(__dirname, './.env') })
 
