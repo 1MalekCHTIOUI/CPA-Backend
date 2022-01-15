015eb995dd1f1e6919c3dacff0a3ff7c6feb0b47
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-15 15:50:15 +0100

    changes

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ require("dotenv").config({ path: path.resolve(__dirname, './.env') })
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
 .catch((err) => console.log(err));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 app.use('/api/auth', authRouter);
 
 app.listen(PORT, () =>{
