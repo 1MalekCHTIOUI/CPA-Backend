b8344f87bb2df278ea2306080911c21e2f360161
Malek Chtioui <chtiouimalek001@gmail.com>
2022-01-18 15:39:55 +0100

    Testing remotely | backend

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 require("dotenv").config({ path: path.resolve(__dirname, './.env') })
 
 //REMOTE MONGO URI
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
-// .catch((err) => console.log("MongoDB Not Connected"))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
+.catch((err) => console.log("MongoDB Not Connected"))
 
 //LOCAL
-mongoose.connect("mongodb://127.0.0.1:27017/cpa", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
-.catch((err) => console.log("MongoDB Not Connected"))
+// mongoose.connect("mongodb://127.0.0.1:27017/cpa", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB has been connected"))
+// .catch((err) => console.log("MongoDB Not Connected"))
 
 app.use('/api/auth', authRouter);
 
